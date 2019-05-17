c4cc7c5d8f658470fdcd0b2fcd712909ee411edc
UGeneF <evgeny.firstoff@yandex.ru>
2019-05-17 20:11:54 +0500

    Extracted several methods.

--- KnapsackAlgorithm/Program.cs
+++ KnapsackAlgorithm/Program.cs
@@ -6,29 +6,41 @@ namespace KnapsackAlgorithm
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Exit on Ctrl-C
             while (true)
             {
                 Console.WriteLine("Enter the backpack capacity: ");
-                var capacity = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter item count: ");
-                var itemCount = int.Parse(Console.ReadLine());
-                var items = new List<Item>();
-                for (var i = 0; i < itemCount; i++)
-                {
-                    Console.Write("Enter item weight, cost: ");
-                    var str = Console.ReadLine().Split(' ');
-                    items.Add(new Item(int.Parse(str[0]), double.Parse(str[1])));
-                }
+                var capacity = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());               
+                var items = ReadItemsFromConsole();
 
-                Console.WriteLine("Capacity: " + capacity);
-                for (int i = 0; i < items.Count; i++)
-                    Console.WriteLine($"{i + 1}: {items[i].Weight}kg {items[i].Cost}$");
-                var result = PackingAlgorithm.GetMaxPossibleCost(items, new Knapsack(capacity));
-                Console.WriteLine($"Result: {result}$");
+                WriteResultToConsole(capacity, items);
             }
+        }
+
+        private static void WriteResultToConsole(int capacity, List<Item> items)
+        {
+            Console.WriteLine("Capacity: " + capacity);
+            for (int i = 0; i < items.Count; i++)
+                Console.WriteLine($"{i + 1}: {items[i].Weight}kg {items[i].Cost}$");
+            var result = PackingAlgorithm.GetMaxPossibleCost(items, new Knapsack(capacity));
+            Console.WriteLine($"Result: {result}$");
+        }
+
+        private static List<Item> ReadItemsFromConsole()
+        {
+            Console.WriteLine("Enter item count: ");
+            var itemCount = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            var items = new List<Item>();
+            for (var i = 0; i < itemCount; i++)
+            {
+                Console.Write("Enter item weight, cost: ");
+                var str = Console.ReadLine()?.Split(' ');
+                if (str != null) items.Add(new Item(int.Parse(str[0]), double.Parse(str[1])));
+            }
+
+            return items;
         }
     }
 }
